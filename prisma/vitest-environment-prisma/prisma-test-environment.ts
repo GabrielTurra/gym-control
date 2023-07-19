3291552ebec26eb2285ee16b022713292242562b
Gabriel Turra <gabriel.turra@outlook.com>
2023-07-19 12:46:29 -0300

    test(e2e): add tests e2e for controllers

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,38 @@
+import "dotenv/config";
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string){ 
+  if(!process.env.DATABASE_URL){
+    throw new Error("Please provide a database url!");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default<Environment> {
   name: "prisma",
   async setup() {
-    console.log("🤖 Setup tests E2E");
+    const schema = randomUUID();
+    const database = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = database;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+        await prisma.$disconnect;
       },
     };
   }
