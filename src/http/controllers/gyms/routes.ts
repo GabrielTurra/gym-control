b2a244becc372b38a3b89d106104bc5ce68fc717
Gabriel Turra <gabriel.turra@outlook.com>
2023-07-19 16:59:08 -0300

    feat(rbac): add RBAC authorization in app with tests adjustments

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { search } from "./search.controller";
 import { nearby } from "./nearby.controller";
 import { create } from "./create.controller";
 import { VerifyJWT } from "@/http/middlewares/verify-jwt";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", VerifyJWT);
 
-  app.post("/gyms", create);
+  app.post("/gyms", {onRequest: [verifyUserRole("ADMIN")]}, create);
 
   app.get("/gyms/search", search);
   app.get("/gyms/nearby", nearby);
